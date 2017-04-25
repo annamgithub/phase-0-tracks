f40ec18329a04d7cc0c520ac1bd0dc9a1d4a4e17
Anna M <annamgithub@gmail.com>
2017-04-25 11:56:19 -0400

    Create desk containing items

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,35 @@
+# In this exercise we shall assume I have a desk with two drawers, each with various items inside.
+
+
+desk = {
+  desktop: {
+    total_item_count: 16,
+    pencil_count_in_jar: 10,
+    other_items: 6
+    },
+    desktop_items: [
+      'pencil jar', 
+      'laptop', 
+      'extra monitor', 
+      'external keyboard', 
+      'notebook', 
+      'coffee cup'
+    ],
+
+  drawer_1: {
+    total_item_count: 3
+    },
+    drawer_1_items: [
+      '3 empty notebooks'
+    ],
+
+  drawer_2: {
+    total_item_count: 2
+    },
+    drawer_2_items: [
+      'empty envelope',
+      'fountain pen'
+    ]
+}
+
+
