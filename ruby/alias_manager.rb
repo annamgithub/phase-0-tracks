5863f2e66201185d41444a98b290c26a23ca4348
Anna M <annamgithub@gmail.com>
2017-04-26 02:14:05 -0400

    5.5 Solo Challenge - draft 1

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,22 @@
+# Swapping the first and last name.
+
+puts "What is your full name?"
+  full_name = gets.chomp
+  
+def swap(full_name)
+  full_name.split(" ")
+  puts "#{full_name.split(" ")[1]} #{full_name.split(" ")[0]}"
+end
+
+swap(full_name)
+
+
+# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+
+vowels = "aeiou"
+consonants = "bcdfghjklmnpqrstvwxyz"
+
+puts "What is your full name?"
+  full_name = gets.chomp
+
+def vowel_change()
