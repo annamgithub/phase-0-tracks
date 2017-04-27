b75bdc901e1d2e0cc658865356dfab10a32fe347
Anna M <annamgithub@gmail.com>
2017-04-27 12:01:03 -0400

    Experiment with letters

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,24 +11,38 @@ end
 swap(full_name)
 
 
-# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
-
-vowels = "aeiou"
-consonants = "bcdfghjklmnpqrstvwxyz"
-
-vowel_array = vowels.split("")
-consonants.split("")
-
-vowels.each do |nextvowel|
-  nextvowel = vowel_array.next
-end
-
-p nextvowel
-
-
-
+# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet.to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
 
 puts "What is your full name?"
   full_name = gets.chomp
 
-def vowel_change()
+vowels = "aeiou".split("")
+new_vowel = vowels.map! {|letter| letter.next}
+
+consonants = "bcdfghjklmnpqrstvwxyz".split("")
+new_consonant = consonants.map! {|letter| letter.next}
+
+full_name_letters = full_name.split("")
+
+if (full_name_letters.include? vowels)
+  alias_vowel = new_vowel
+elsif (full_name_letters.include? consonants)
+  alias_consonant = new_consonant
+end
+
+puts alias_vowel
+puts alias_consonant
+
+
+
+
+
+
+
+
+
+
+# puts "What is your full name?"
+#   full_name = gets.chomp
+
+# def vowel_change()
