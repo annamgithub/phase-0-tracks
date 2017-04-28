4cb66ee86958954598d521c8c68fcef46f4ba079
Anna M <annamgithub@gmail.com>
2017-04-28 14:31:08 -0400

    Build alias-generator

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,46 +1,20 @@
-# Swapping the first and last name.
+# Release 0, Step 1. Swap first and last names
 
 puts "What is your full name?"
   full_name = gets.chomp
   
 def swap(full_name)
   full_name.split(" ")
-  puts "#{full_name.split(" ")[1]} #{full_name.split(" ")[0]}"
+  swapped_name = "#{full_name.split(" ")[1]} #{full_name.split(" ")[0]}"
+  puts swapped_name.tr('AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz', 'EIOUAeiouaCDFGHJKLMNPQRSTVWXYZBcdfghjklmnpqrstvwxyzb')
 end
 
-swap(full_name)
+puts swap(full_name)
 
 
-# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet.to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+# Swapping first and last names given by: 
+# "#{full_name.split(" ")[1]} #{full_name.split(" ")[0]}"
 
 
-vowels = "aeiou".split("")
-new_vowel = vowels.map! {|letter| letter.next}
+# Release 0, Step 2. Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
 
-consonants = "bcdfghjklmnpqrstvwxyz".split("")
-new_consonant = consonants.map! {|letter| letter.next}
-
-full_name_letters = full_name.split("")
-
-if (full_name_letters.include? vowels)
-  alias_vowel = new_vowel
-elsif (full_name_letters.include? consonants)
-  alias_consonant = new_consonant
-end
-
-puts alias_vowel
-puts alias_consonant
-
-
-
-
-
-
-
-
-
-
-# puts "What is your full name?"
-#   full_name = gets.chomp
-
-# def vowel_change()
