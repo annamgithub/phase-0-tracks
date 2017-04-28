2f9960b85e9fc5a4b024314b4a7a14ec442f24d4
Anna M <annamgithub@gmail.com>
2017-04-28 15:41:22 -0400

    Add user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,20 +1,31 @@
-# Release 0, Step 1. Swap first and last names
 
-puts "What is your full name?"
-  full_name = gets.chomp
+puts "Welcome to the Alias Generator. Let's get started. Please enter your first and last names."
+
+full_name = gets.chomp
+
+  def new_alias(full_name)
+    full_name.split(" ")
+    swapped_name = "#{full_name.split(" ")[1]} #{full_name.split(" ")[0]}"
+    puts swapped_name.tr('AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz', 'EIOUAeiouaCDFGHJKLMNPQRSTVWXYZBcdfghjklmnpqrstvwxyzb')
+  end
   
-def swap(full_name)
-  full_name.split(" ")
-  swapped_name = "#{full_name.split(" ")[1]} #{full_name.split(" ")[0]}"
-  puts swapped_name.tr('AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz', 'EIOUAeiouaCDFGHJKLMNPQRSTVWXYZBcdfghjklmnpqrstvwxyzb')
+puts new_alias(full_name)
+
+while full_name != "quit"
+  puts "Want another one? Please enter another set of first and last names you wish to create an alias out of. Otherwise, you can quit at any time by entering 'quit'."
+  full_name = gets.chomp
+
+  def new_alias(full_name)
+    full_name.split(" ")
+    swapped_name = "#{full_name.split(" ")[1]} #{full_name.split(" ")[0]}"
+    puts swapped_name.tr('AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz', 'EIOUAeiouaCDFGHJKLMNPQRSTVWXYZBcdfghjklmnpqrstvwxyzb')
+  end
+  
+  if full_name != "quit" 
+    puts new_alias(full_name)
+  elsif full_name == "quit"
+    break 
+  end
 end
 
-puts swap(full_name)
-
-
-# Swapping first and last names given by: 
-# "#{full_name.split(" ")[1]} #{full_name.split(" ")[0]}"
-
-
-# Release 0, Step 2. Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
 
