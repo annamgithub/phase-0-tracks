3644ab3a95251908ed3b9053aa84ab5e60971345
Anna M <annamgithub@gmail.com>
2017-04-29 14:56:49 -0400

    Include completion of hashes section

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -15,22 +15,8 @@ zombie.each {|x| print "#{x}*"}
 alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
 z = zombie.each {|item| item.split("")}
 
-# def sort(arr)
-#   x = 0
-#   while x < arr.length
-#     y = x + 1 
-#     while y < arr.length
-#       if arr[x] < arr[y]
-#         arr[x], arr[y] = arr[y]
-#       end
-#       y += 1
-#     end
-#     x += 1
-#   end
-#   return arr.reverse.join(' ')
-# end
+# need to ask about this
 
-# sort(zombie)
 
 # ----
 
@@ -94,20 +80,25 @@ extinct_animals = {
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
 
-
-
-
+extinct_animals.each {|animal, date| puts "#{animal}-#{date}*"}
 
 # ----
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
+
+extinct_animals.keep_if {|animal, date| date < 2000} 
+
 # ----
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
+
+extinct_animals.each {|animal, date| p animal, date-3}
+
+
 # ----
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
@@ -116,10 +107,29 @@ extinct_animals = {
 # "Dodo"
 # "Saiga Antelope"
 # Do not use any special built-in methods.
+
+extinct_animals.include?("Andean Cat")
+extinct_animals.include?("Dodo")
+extinct_animals.include?("Saiga Antelope")
+
+
+
 # ----
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
+
+
+extinct_animals.delete("Passenger Pigeon")
+puts extinct_animals
+
+extinct_animals.keep_if {|animal, date| animal == "Passenger Pigeon"}
+extinct_animals.to_a
+
+
+
+
+
 # ----
