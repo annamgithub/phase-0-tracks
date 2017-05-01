a4412cedeac60dfc164ef08503d96a3234082278
Anna M <annamgithub@gmail.com>
2017-05-01 18:42:52 -0400

    Release 0

---
+++ ruby/santa.rb
@@ -0,0 +1,18 @@
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
+  def initialize
+    puts "Initializing Santa instance..."
+  end
+
+
+# Driver Code
+
+initialize
+speak
+eat_milk_and_cookies("Oreo")
