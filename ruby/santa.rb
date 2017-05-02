a34b4af84a9819681818205e3c1003449ade6702
Anna M <annamgithub@gmail.com>
2017-05-02 17:39:46 -0400

    complete Santa exercise

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,13 @@
 class Santa
 
+  attr_reader :age, :ethnicity
+  attr_accessor :gender
+
+
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
-    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
 
@@ -34,50 +37,62 @@ class Santa
     @age += 1
   end
 
-  def get_mad_at(reindeer)
-    i = 0
-    while naughty_reindeer != @reindeer_ranking[i]
-      if naughty_reindeer == @reindeer_ranking[i]
-        naughty_reindeer = @reindeer_ranking.delete[i]
-        @reindeer_ranking << naughty_reindeer
-      end
-      i += 1
+  def get_mad_at(naughty_reindeer)
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @reindeer_ranking.delete(naughty_reindeer)
+    @reindeer_ranking << naughty_reindeer
+    p @reindeer_ranking
     end
-  end
 end
 
-#Setter Method
-  def gender=(gender)
-    @gender = gender
-  end
+# #Setter Method
+#   def gender=(gender)
+#     @gender = gender
+#   end
 
-#Getter Method
-  def age
-    @age
-  end
+# #Getter Method
+#   def age
+#     @age
+#   end
 
-  def ethnicity
-    @ethnicity
-  end
-  
-# Driver Code
+#   def ethnicity
+#     @ethnicity
+#   end
 
-santas = []
+
+
+#__________________________________________
+
+# *** Driver Code ***
+
+# santas = []
 gender = ["male", "female", "other"]
 ethnicity = ["Black", "White", "Asian", "Latino", "Mixed", "Other"]
 
-gender.length.times do |i|
-ethnicity.length.times do |x|  
+# gender.length.times do |i|
+# ethnicity.length.times do |x|  
 # santas << Santa.new(gender[i], ethnicity[x])
 
-santa = Santa.new(gender[i], ethnicity[x])
-santa.speak
-santa.eat_milk_and_cookies("Oreo")
-santa.about
+# santa = Santa.new(gender[i], ethnicity[x])
+# santa.speak
+# santa.eat_milk_and_cookies("Oreo")
+# santa.about
+# santa = Santa.new("female", "Mixed")
+# santa.celebrate_birthday
 
-end
-end
+# @reindeer_ranking
+# santa.get_mad_at("Vixen")
+# @reindeer_ranking
 
+# end
+# end
+
+300.times do |santa|
+  age = rand(0...140)
+  santa_gender = gender.sample
+  santa_ethnicity = ethnicity.sample
+  p "Santa's age: #{age}. Santa's gender: #{santa_gender}. Santa's ethnicity: #{santa_ethnicity}"
+end
 
 
 
