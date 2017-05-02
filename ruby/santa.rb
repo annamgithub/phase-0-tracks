3eb9f631a9d0d93327c1258f7a4268ef04cfc4a6
Anna M <annamgithub@gmail.com>
2017-05-02 16:45:44 -0400

    Complete up to release 2 inclusive

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,12 +1,11 @@
-  class Santa
-
-@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-@age = 0
+class Santa
 
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -24,11 +23,32 @@
   def get_mad_at(name)
     reindeer_ranking[name].delete
     reindeer_ranking[-1].push
+  end
+  
+  def about
+    puts "Gender: #{@gender}"
+    puts "Ethnicity: #{@ethnicity}"
+  end
+  
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    i = 0
+    while naughty_reindeer != @reindeer_ranking[i]
+      if naughty_reindeer == @reindeer_ranking[i]
+        naughty_reindeer = @reindeer_ranking.delete[i]
+        @reindeer_ranking << naughty_reindeer
+      end
+      i += 1
+    end
+  end
 end
 
 #Setter Method
-  def gender
-    @gender
+  def gender=(gender)
+    @gender = gender
   end
 
 #Getter Method
@@ -40,20 +60,24 @@ end
     @ethnicity
   end
   
-
-
 # Driver Code
 
-santa = Santa.new
+santas = []
+gender = ["male", "female", "other"]
+ethnicity = ["Black", "White", "Asian", "Latino", "Mixed", "Other"]
+
+gender.length.times do |i|
+ethnicity.length.times do |x|  
+# santas << Santa.new(gender[i], ethnicity[x])
+
+santa = Santa.new(gender[i], ethnicity[x])
 santa.speak
 santa.eat_milk_and_cookies("Oreo")
+santa.about
 
-# santas = []
-# gender = ["male", "female", "other"]
-# ethnicity = ["white", "black", "East Asian", "South Asian", "Latino", "Mixed", "Other"]
-# gender.length.times do |i|
-# santas << Santa.new(gender[i], ethnicity[i])
-# end
+end
+end
+
 
 
 
