ec61c33e4548100c8fea52f1accbced0050287a9
Anna M <annamgithub@gmail.com>
2017-05-03 10:52:56 -0400

    Add mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,40 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yelling_happily(words)
-    words + "!!!!!!" + " :)"
+#   def self.yelling_happily(words)
+#     words + "!!!!!!" + " :)"
+#   end
+# end
+
+
+# # Driver Code
+
+# Shout.yell_angrily("Darn it")
+# Shout.yelling_happily("Yay")
+
+#________________________
+
+
+module Shout
+  def yell(words)
+    puts words + "!!!" 
   end
 end
 
+class Austin_Powers
+  include Shout
+end
+
+class Agent_Kensington
+  include Shout
+end
 
 # Driver Code
 
-Shout.yell_angrily("Darn it")
-Shout.yelling_happily("Yay")
+powers = Austin_Powers.new
+powers.yell("Yeah, baby, yeah")
+
+kensington = Agent_Kensington.new
+kensington.yell("Oh, behave")
