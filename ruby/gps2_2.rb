ed7eb60df4051a3b7667abd02dfbf855fc1d273e
Anna M <annamgithub@gmail.com>
2017-05-04 11:51:56 -0400

    Complete GPS 2_2.rb

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,8 +1,12 @@
+# RELEASE 0 & 1
+
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
 # Convert string into separate keys
 # Set quantity (value) for each key to 1
 # print the list to the console [can you use one of your other methods here?]
+# output: Hash
 
 def create_list(item_list)
   list_arr = item_list.split(' ')
@@ -16,7 +20,7 @@ end
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
-# steps: Add the item to the list of keys in the hash
+# steps: Add the item to the list of keys in the hash, with default quantity as 1.
 # output: Print the list with the addition
 
 def add_item_to_list(grocery_list, item)
@@ -28,7 +32,7 @@ end
 # Method to remove an item from the list
 # input: list, item name, and optional quantity
 # steps: delete identified item from hash
-# output: list without the deleted item
+# output: Print list without the deleted item
 
 def remove_item_from_list(grocery_list, item) 
   grocery_list.delete(item)  
@@ -38,11 +42,11 @@ end
 
 # Method to update the quantity of an item
 # input: list, item, quantity
-# steps: change quantity (value) of item, convert string to integer
+# steps: change quantity (value) of item (key), convert string to integer
 # output: list reflecting the new quantity
 
 def update_quantity(grocery_list, item, quantity)
-  grocery_list[item] = quantity.to_i 
+  grocery_list[item] = quantity
   grocery_list
 end 
 
@@ -55,15 +59,47 @@ end
 def print_finished_list(grocery_list)
   puts "Here is your grocery list: "
   grocery_list.each do |item, quantity|
-    puts "Buy #{quantity} #{item}" 
+    puts "#{quantity} #{item}(s)" 
   end
 end 
 
 
-# USER INTERFACE
+# RELEASE 2 & 3 - Testing Solution & Refactoring (covering entire document)
 
-puts "What items do you need to buy? Type a list separated by spaces."
-list_input = gets.chomp
-grocery_list = create_list(list_input)
-print_finished_list(grocery_list)
+grocery_list = create_list("carrots apples cereal pizza")
+add_item_to_list(grocery_list, "Lemonade") 
+add_item_to_list(grocery_list, "Tomatoes")
+add_item_to_list(grocery_list, "Onions")
+add_item_to_list(grocery_list, "Ice Cream")
+update_quantity(grocery_list, "Lemonade", 2)
+update_quantity(grocery_list, "Tomatoes", 3)
+update_quantity(grocery_list, "Ice Cream", 4)
+
+remove_item_from_list(grocery_list, item) 
+update_quantity(grocery_list, "Ice Cream", 1)
+
+p grocery_list
+
+
+# RELEASE 4 - Reflect
+# Pseudocode was useful for laying out all the intentions steps of my code in the form of an initial blueprint without having to conform to the ruby syntax.
+# Using just an array would have been attractive due the greater simplicity compared to a hash, but an array would not have structured the data as I would have liked in this exercise to include both a key and a value.
+# A method returns values. 
+# You can pass various objects into methods as arguments including strings, integers, arrays, hashes, etc.
+# You can pass information between methods by assigning it to a variable and using the variable in other methods.
+# This was a good exercise for practising hashes and passing information across different methods. This is still new to me so I will need further practice to solidify these skills.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
