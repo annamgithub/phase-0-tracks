3d41667a0d11816d1de298c44c7e94c73840acc1
Anna M <annamgithub@gmail.com>
2017-05-05 23:00:42 -0400

    Write class & modify code for rspec testing

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,21 +1,24 @@
+require_relative 'TodoList'
+
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
-  it "stores the list items given on initialization" do
-    expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
-  end
+  # it "stores the list items given on initialization" do
+  #   expect(TodoList.new).to eq ["do the dishes", "mow the lawn"]
+  # end
 
   it "adds an item to the list" do
-    list.add_item("mop")
-    expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
+    expect(list.add("mop")).to eq ["do the dishes", "mow the lawn", "mop"]
   end
 
   it "deletes an item" do
-    list.delete_item("do the dishes")
-    expect(list.get_items).to eq ["mow the lawn"]
+  #   # list.delete("do the dishes")
+    expect(list.del("do the dishes")).to eq ["mow the lawn"]
   end
 
   it "retrieves an item by index" do
-    expect(list.get_item(0)).to eq "do the dishes"
+    expect(list.retrieve(0)).to eq "do the dishes"
   end
+
+
 end
