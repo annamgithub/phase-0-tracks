4c3533533ff1f18663ff6480d9b1bec132077c0e
Anna M <annamgithub@gmail.com>
2017-05-06 14:27:45 -0400

    Make all methods in class pass rspec tests.

--- ruby/game_spec.rb
+++ ruby/game_spec.rb
@@ -7,4 +7,15 @@ it "prints underscores according to the length of the word" do
   expect(word.underscore("challenge")).to eq 9 
 end
 
+it "sets number of guesses to the length of the word" do
+  expect(word.guessing("challenge")).to eq 9
+end
+
+it "checks if inputted letter is included in set word" do
+  expect(word.inclusion("c")).to eq true
+end
+
+
+
+
 end
--- ruby/word_game.rb
+++ ruby/word_game.rb
@@ -1,12 +1,11 @@
 class Word_Game
-  attr_writer :set_word
+  attr_accessor :set_word
 
 # Initialize the game. In this section, have the word_setter set the word. 
 # Set the number of guesses to the number of letters in the word.
 
   def initialize(set_word)
     @set_word = set_word 
-
   end
 
 # Print underscores to be seen by the word_guesser. The number of underscores should match the letter length of the word_setter’s word.
@@ -17,21 +16,32 @@ class Word_Game
     end
   end
 
-# Ensure that in driver code the guessers guesses are inputted. Check whether guessed letter is within word_setter’s word. (.include?)
-
-  # def guessing(set_word)
-  #   i = @num_guesses
-
-  # end
+  def guessing(set_word)
+    @set_word.length
+  end
 
 # If it is included, print the set of underscores but replace the underscores representing the place of the correctly guessed letter.
-# If it is not included, tell the user that she is incorrect and set counter to 1 less that previous guess count. 
-# Continue with process until guess counter reaches zero or until all the underscores are replaced with letters. 
-# Congratulate user if underscores are all replaced by letters.
-# If counter reaches zero, print a taunting message to the user.
+
+  def inclusion(letter)
+    @set_word.include?(letter)
+  end
+
 end
 
 
 # Driver Code
-game = Word_Game.new("challenge")
-game.underscore("challenge")
+# game = Word_Game.new("challenge")
+# game.underscore("challenge")
+# puts "You have #{@set_word.length} guesses remaining."
+
+# Inclusion
+    # puts "Please enter a letter you think might be in the word."
+    # letter = gets.chomp
+
+    # num_guesses -= 1
+
+    # If it is not included, tell the user that she is incorrect and set counter to 1 less that previous guess count. 
+# Continue with process until guess counter reaches zero or until all the underscores are replaced with letters. 
+
+# Congratulate user if underscores are all replaced by letters.
+# If counter reaches zero, print a taunting message to the user.
