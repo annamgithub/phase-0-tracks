593de64dbe44c14d40e3651fa0ac24a717f29325
Anna M <annamgithub@gmail.com>
2017-05-09 12:30:03 -0400

    Complete 7.2 pairing

--- js/data_structures.js
+++ js/data_structures.js
@@ -19,3 +19,44 @@ console.log(profiles)
 
 
 //Release 2
+
+function Cars(type, color, isElectric, mpg) {
+  this.type = type;
+  this.color = color;
+  this.isElectric = isElectric
+  this.mpg = mpg;
+
+  this.isItGreen = function () {
+    if (this.color == "green" || this.isElectric == true) {
+        return "yes"
+    } else {
+        return "no"
+    } 
+    console.log(this.color + this.isElectric);
+  }
+}
+
+var newCar = new Cars("sedan", "red", true, 32);
+var newCar2 = new Cars("truck", "blue", false, 32);
+var newCar3 = new Cars("jeep", "black", false, 55);
+
+console.log(newCar)
+console.log(newCar.isItGreen());
+console.log("CAR CREATION COMPLETE!")
+
+console.log(newCar2)
+console.log(newCar2.isItGreen());
+console.log("CAR CREATION COMPLETE!")
+
+console.log(newCar3)
+console.log(newCar3.isItGreen());
+console.log("CAR CREATION COMPLETE!")
+
+
+
+
+
+
+
+
+
