81bc0777d7194ab1b707223cd284221a49ceab1b
Anna M <annamgithub@gmail.com>
2017-05-09 11:15:57 -0400

    Complete release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,7 +1,21 @@
-var colors = ["blue", "green", "yellow", "red"];
+//Release 0 
 
-var names = ["Ed", "John", "Alison", "Zelda"];
+var colors = ["blue", "green", "yellow", "red"];
+var names = ["Ed", "John", "Alison", "Alex"];
 
 colors.push("purple");
-
 names.push("Harry");
+
+
+//Release 1
+
+var profiles = {}
+
+for (var i = 0; i < names.length; i++) {
+  profiles[names[i]] = colors[i];
+}
+
+console.log(profiles)
+
+
+//Release 2
