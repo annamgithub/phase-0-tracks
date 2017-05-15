a38b9023c22e4d7fd9f39ced95ef9d4793c0323b
Anna M <annamgithub@gmail.com>
2017-05-15 00:51:53 -0400

    Modify release 0 and complete release 2

--- js/algos.js
+++ js/algos.js
@@ -1,29 +1,52 @@
 // Task: Write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
 
-// Pseudocode:
+// Release 0 Pseudocode:
 // Create a function to determine the length of each phrase in the array.
 // Check if the first phrase is longer than the other phrases.
 // If it is not longer, move on to the next phrase in the array and check to see whether it is longer than the other phrases in the array.
 // Continue until the longest phrase is identified, then print it.
 
-
-function phrases([p1, p2, p3]) {
-  if (p1.length > p2.length && p3.length) {
-    console.log(p1)
-  }
-
-  else if (p2.length > p1.length && p3.length) {
-    console.log(p2)
-  }
-
-  else if (p3.length > p1.length && p2.length) {
-    console.log(p3)
-  }
-}
-
+function phrases(p) {
+   var longest_phrase = p[0];
+    for (var i = 0; i< p.length; i++) {
+       if (p[i].length > longest_phrase.length) {
+         longest_phrase = p[i];
+       }
+     }
+     return longest_phrase;
+ }
 
 //Driver Code
 phrases(["Oh, behave!", "Yeah, baby, yeah!", "Get in my belly!", "I also like to live dangerously"])
 phrases(["long phrase","longest phrase","longer phrase", "this is the longest phrase ever"])
+
+
+
+//Release 1 Pseudocode
+// Write a function that takes two objects and checks to see if the objects share at least one key-value pair.
+
+function favorite_food(item1, item2) {
+   for (var k1 in item1) {
+     for (var k2 in item2) {
+       if (k1 === k2) {
+         if (item1[k1] === item2[k2]) {
+           return true;
+         } 
+       }
+     }
+   }
+   return false;
+ }
+ 
+//Driver Code
+favorite_food({name: "Anna", food: "apple pie"}, {name: "Miranda", food: "apple pie"}); 
+favorite_food({name: "Christa", food: "apple pie"}, {name: "Miranda", food: "Sichuan chicken"}); 
+
+
+
+
+
+
+
 
 
